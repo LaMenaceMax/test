ce02c6194b2147f2e7510074a7b7d3717f701ce3
LaMenaceMax <maxiusmaxius@hotmail.com>
2017-12-04 10:48:33 -0500

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,24 @@
+// Test.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include "gtest/gtest.h"
+
+int additionner(int x, int y)
+{
+	return x + y;
+}
+
+
+TEST(UnitTest, additionner1)
+{
+	int x = additionner(2, -2);
+	EXPECT_EQ(0, x);
+}
+
+int main(int argc,char* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
